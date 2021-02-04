6034d5292127127f9fe29c346af2fde26673bfc5
Will Garcia <Will.Garcia@bmwgroup.com>
2021-02-04 11:27:46 -0500

    bump versions

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "10.0.0"
+  version = "14.0.0"
 
   cluster_name                               = var.cluster_prefix
   subnets                                    = concat(var.private_subnets, var.public_subnets)
